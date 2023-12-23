b88e3887ba3ac76cfd0c44678d6d58de4efaa90f
jethrolibutan <jethrolibutan@gmail.com>
2023-12-22 21:41:31 -0500

    first edits to about me section

---
+++ src/pages/AboutMe.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./AboutMe.css";
+
+function AboutMe() {
+  return (
+    <div className="about-me-section">
+      <div className="about-me-picture">INSERT PICTURE HERE</div>
+
+      <div className="about-me">
+        <div className="about-me-text">About Me</div>
+        <div className="jethro-text">
+          I am Jethro Libutan. I am a computer science student at East Carolina
+          University. I enjoy creating software and bringing my ideas and
+          other's ideas to life. Constantly immersed in both front-end and
+          back-end development, I am dedicated to expanding my skill set and
+          acquiring new knowledge. My aim is to become a versatile engineer,
+          capable of navigating the complexities of both client-side and
+          server-side development. My primary experience is mostly in full stack
+          web applications. I also have experience in mobile development and
+          marketing.
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AboutMe;
